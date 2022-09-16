4be65b1d5e18167aa70ed4fe86ab85e844b52dfb
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-16 21:47:21 +0900

    loginform

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import palette from "../../lib/styles/palette";
 
 const StyyledButton = styled.button`
@@ -14,6 +14,22 @@ const StyyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+  ${(props) =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
 `;
 
 const Button = (props) => <StyyledButton {...props} />;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,12 @@
+import AuthTemplate from "../components/auth/AuthTemplate";
+import AuthForm from "../components/auth/AuthForm";
+
 const LoginPage = () => {
-  return <div>로그인</div>;
+  return (
+    <AuthTemplate>
+      <AuthForm />
+    </AuthTemplate>
+  );
 };
 
 export default LoginPage;
